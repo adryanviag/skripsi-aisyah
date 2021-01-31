d7a6c91721e3d4fd9804464396fd50e1b4b98d92
adryan <75743805+adryanviag@users.noreply.github.com>
2021-01-31 13:26:04 +0700

    hashtag #sawitbaik

--- index.py
+++ index.py
@@ -2,13 +2,12 @@ import twint
 
 # Configure
 c = twint.Config()
-c.Search = "fpi ditembak"
+c.Search = "#sawitbaik"
 c.Lang = "in"
 #! c.Since = "2020-01-01"
-#! c.Until = "2020-04-25"
-c.Limit = 500
+c.Until = "2019-09-16"
 
-c.Output = "./test.csv"
+c.Output = "./sawit_baik.csv"
 c.Store_csv = True
 c.Count = True
 
